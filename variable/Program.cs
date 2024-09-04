605e65ac93831958001bf95c1736ae977f012c7d
Ardor <Ardor@DESKTOP-BQF7240>
2024-09-04 13:40:20 +0800

    Добавьте файлы проекта.

---
+++ variable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{ 
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            int flask = 1;
+
+            float money = 2f;
+
+            double percent = 3.5;
+
+            char letter = 'G';
+
+            string name = "Sinking city";
+
+            bool lie = true;
+
+            byte Ships = 105;
+
+            short temperature = -5;
+
+            long gold = 34536;
+
+            ushort amulet = 422;
+        }
+       
+
+    }
+
+}
+
