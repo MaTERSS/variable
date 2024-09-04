2208b6a77e9a663cb06c56e3f1d818a8c4a5aeff
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2024-09-04 17:36:09 +0800

    Update Program.cs

--- variable/Program.cs
+++ variable/Program.cs
@@ -7,24 +7,15 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            int flask = 1;
-
+            int flasks = 1;
             float money = 2f;
-
             double percent = 3.5;
-
             char letter = 'G';
-
             string name = "Sinking city";
-
-            bool lie = true;
-
-            byte Ships = 105;
-
+            bool isLie = true;
+            byte ships = 105;
             short temperature = -5;
-
             long gold = 34536;
-
             ushort amulet = 422;
         }
     }
