ca71d47d29174d735ae19c962267ae795039fc35
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2024-09-04 13:47:48 +0800

    Update Program.cs

--- variable/Program.cs
+++ variable/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CSLight
 { 
@@ -30,9 +27,6 @@ namespace CSLight
 
             ushort amulet = 422;
         }
-       
-
     }
-
 }
 
